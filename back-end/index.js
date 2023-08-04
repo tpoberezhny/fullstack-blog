d524c7871e50af3f02004e36c0db8555bb1bb20d
Tim <poberezhny003@gmail.com>
2023-08-04 16:25:10 +0200

    Encrypting received passwords, giving token, try/catch

--- back-end/index.js
+++ back-end/index.js
@@ -1,17 +1,20 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { validationResult } from 'express-validator';
+import { validationResult } from "express-validator";
 import { registerValidation } from "./validations/auth.js";
+
+import UserModel from "./models/User.js";
 
 const adminPassword = process.env.MONGO_ADMIN_PASSWORD;
 
 mongoose
   .connect(
-    `mongodb+srv://admin:${adminPassword}@cluster0.m4jtwh0.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://admin:${adminPassword}@cluster0.m4jtwh0.mongodb.net/blog?retryWrites=true&w=majority`
   )
   .then(() => console.log("DB ok"))
   .catch((err) => console.log("DB error", err));
@@ -20,15 +23,50 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array())
-  }
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
 
-  res.json({
-    success: true
-  })
+    //Encrypting the received password
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+    
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Registration failed",
+    });
+  }
 });
 
 app.listen(4444, (err) => {
