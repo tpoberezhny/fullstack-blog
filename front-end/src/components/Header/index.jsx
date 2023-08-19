6c53f985795e71b617de13243baece6e0c3e5ec5
Tim <poberezhny003@gmail.com>
2023-08-20 01:43:10 +0200

    Log out functionality

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 
 import styles from "./Header.module.scss";
 import Container from "@mui/material/Container";
-import { useSelector } from "react-redux";
-import { selectIsAuth } from "../../redux/slices/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectIsAuth } from "../../redux/slices/auth";
 
 export const Header = () => {
+  const dispatch = useDispatch();
   const isAuth = useSelector(selectIsAuth);
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if (window.confirm("Are you sure that you want log out?"))
+      dispatch(logout());
+  };
 
   return (
     <div className={styles.root}>
