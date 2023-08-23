af2686b24d79aa8db2d3a1345e6ebec1fd1f4d75
Tim <poberezhny003@gmail.com>
2023-08-23 02:26:20 +0200

    Registration form

--- front-end/src/pages/Registration/index.jsx
+++ front-end/src/pages/Registration/index.jsx
@@ -4,10 +4,45 @@ import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
+import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
+import { Navigate } from "react-router-dom";
 
 import styles from "./Login.module.scss";
 
 export const Registration = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      fullname: "Lovely User",
+      email: "user@test.com",
+      password: "12345",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values));
+
+    if (!data.payload) {
+      return alert("Can not create account");
+    }
+
+    if ("token" in data.payload) {
+      window.localStorage.setItem("token", data.payload.token);
+    }
+  };
+
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
@@ -16,12 +51,37 @@ export const Registration = () => {
       <div className={styles.avatar}>
         <Avatar sx={{ width: 100, height: 100 }} />
       </div>
-      <TextField className={styles.field} label="Full Name" fullWidth />
-      <TextField className={styles.field} label="E-Mail" fullWidth />
-      <TextField className={styles.field} label="Password" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Log in
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          error={Boolean(errors.fullname?.message)}
+          helperText={errors.fullname?.message}
+          {...register("fullname", { required: "Fill in your Full Name" })}
+          className={styles.field}
+          label="Full Name"
+          fullWidth
+        />
+        <TextField
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          type="email"
+          {...register("email", { required: "Fill in your e-mail" })}
+          className={styles.field}
+          label="E-Mail"
+          fullWidth
+        />
+        <TextField
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          type="password"
+          {...register("password", { required: "Fill in your password" })}
+          className={styles.field}
+          label="Password"
+          fullWidth
+        />
+        <Button disabled={!isValid} type="submit" size="large" variant="contained" fullWidth>
+          Create account
+        </Button>
+      </form>
     </Paper>
   );
 };
