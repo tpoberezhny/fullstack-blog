425ce5b95c9bb204ec0dc346bfbb7b181cb89ffa
Tim <poberezhny003@gmail.com>
2023-08-24 02:01:15 +0200

    Upload image on the server

--- front-end/src/pages/AddPost/index.jsx
+++ front-end/src/pages/AddPost/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useMemo } from "react";
+import { useState, useCallback, useMemo, useRef } from "react";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
@@ -8,14 +8,33 @@ import "easymde/dist/easymde.min.css";
 import styles from "./AddPost.module.scss";
 import { useSelector } from "react-redux";
 import { selectIsAuth } from "../../redux/slices/auth";
+import axios from "../../axios";
 import { Navigate } from "react-router-dom";
 
 export const AddPost = () => {
   const isAuth = useSelector(selectIsAuth);
-  const imageUrl = "";
   const [value, setValue] = useState("");
   const [title, setTitle] = useState("");
   const [tags, setTags] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const inputFileRef = useRef(null);
+
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append("image", file);
+      const { data } = await axios.post("/upload", formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert("Error while trying upload file");
+    }
+  };
+
+  const onClickRemoveImage = () => {
+    setImageUrl('')
+  };
 
   const onChange = useCallback((value) => {
     setValue(value);
@@ -42,9 +61,36 @@ export const AddPost = () => {
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button
+        onClick={() => inputFileRef.current.click()}
+        variant="outlined"
+        size="large"
+      >
         Load preview
       </Button>
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
+      {imageUrl && (
+        <>
+          {" "}
+          <Button
+            variant="contained"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Delete
+          </Button>
+          <img
+            className={styles.image}
+            src={`http://localhost:4444${imageUrl}`}
+            alt="Uploaded"
+          />
+        </>
+      )}
       <br />
       <br />
       <TextField
